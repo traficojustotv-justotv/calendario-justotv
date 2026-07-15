3deaa21166176f0e377d6c4a707498a5d03e93ab
traficojustotv-justotv <traficojustotv@gmail.com>
2026-07-14 20:19:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,36 +3,28 @@ document.addEventListener("DOMContentLoaded", function () {
     // ===============================
     // FECHA Y HORA
     // ===============================
-function actualizarHora() {
+ function actualizarHora() {
 
-    const ahora = new Date();
+        const ahora = new Date();
 
-    const opcionesFecha = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric"
-    };
+        const opcionesFecha = {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        };
 
-    document.getElementById("fecha").innerHTML =
-        ahora.toLocaleDateString("es-ES", opcionesFecha);
+        document.getElementById("fecha").innerHTML =
+            ahora.toLocaleDateString("es-ES", opcionesFecha);
 
-    let horas = ahora.getHours();
-    let minutos = String(ahora.getMinutes()).padStart(2, "0");
-    let segundos = String(ahora.getSeconds()).padStart(2, "0");
+        document.getElementById("hora").innerHTML =
+            ahora.toLocaleTimeString("es-ES");
 
-    let periodo = horas >= 12 ? "PM" : "AM";
-
-    horas = horas % 12;
-
-    if (horas === 0) {
-        horas = 12;
     }
 
-    document.getElementById("hora").innerHTML =
-        `${horas}:${minutos}:${segundos} ${periodo}`;
+    actualizarHora();
 
-}
+    setInterval(actualizarHora,1000);
 
     // ===============================
     // CALENDARIO
